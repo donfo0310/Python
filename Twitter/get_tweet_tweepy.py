f692a1ece5f55cf9717585fca45cb40029c907e3
yoshi <yoshitakaOkada0214@gmail.com>
2019-04-20 14:33:17 +0900

    update

--- Twitter/get_tweet_tweepy.py
+++ Twitter/get_tweet_tweepy.py
@@ -1,14 +1,16 @@
-import  sys
-import  tweepy
+import sys, csv
+import tweepy
 
 from key_tweepy import key_tweepy_proc
 
 api = key_tweepy_proc()
 
 results = api.user_timeline(screen_name="duri0214", count=10)
-#
+
+f = open('output.txt', 'w', encoding='UTF-8', newline='\n')
+writer = csv.writer(f)
+
 for result in results:
-    print(result.id)
-    print(result.created_at)
-    print(result.text)
-    print()
+    writer.writerow([result.text.strip()])
+
+f.close()
