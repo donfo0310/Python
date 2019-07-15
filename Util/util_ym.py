1568c7e9d55fb1d605c839800377fbf6849bb967
yoshi <yoshitakaOkada0214@gmail.com>
2019-07-15 21:31:36 +0900

    pylint update

--- Util/util_ym.py
+++ Util/util_ym.py
@@ -23,19 +23,24 @@ def convert_to_ymd(date_formal):
 
 def count_work_day(yyyymmdd_st, yyyymmdd_en, workday='1111100'):
     """ stからenの範囲のうちworkdayが1の場所をカウントして返す。
-    例えばworkdayの1111100は土日が休みであることを表す"""
-    # ex: between 2019/4/6(sat) and 2019/4/30(tue)
-    ret = (datetime.strptime(yyyymmdd_st, '%Y%m%d'), datetime.strptime(yyyymmdd_en, '%Y%m%d'))
-    # retValue: 00
-    retValue = workday[ret[0].weekday():]
+    例えばworkdayの1111100は土日が休みであることを表す。
+    2019/4/6(土) and 2019/4/30(火) を例に取ったときに、
+    A) 最初の端切れ（土-日）
+    B) 掛け算でパターン計算できる範囲（月-日）
+    C) 最後の端切れ（月-火）
+    と3段階で数字の連結を作ると「00 + (1111100 * multiple) + 11」
+    """
+    day_range = (datetime.strptime(yyyymmdd_st, '%Y%m%d'), datetime.strptime(yyyymmdd_en, '%Y%m%d'))
     # range are 25 days
-    d_range_cnt = (ret[1]-ret[0]).days + 1
-    # retValue: 00 + (1111100 * multiple)
-    multiple = (d_range_cnt - len(retValue)) // 7
-    retValue += workday * multiple
-    # retValue: 00 + (1111100 * multiple) + 11
-    retValue += workday[:(d_range_cnt - len(retValue)) % 7]
-    return retValue.count('1')
+    day_range_cnt = (day_range[1]-day_range[0]).days + 1
+    # A) 00
+    conbine = workday[day_range[0].weekday():]
+    # B) (1111100 * multiple)
+    multiple = (day_range_cnt - len(conbine)) // 7
+    conbine += workday * multiple
+    # C) 11
+    conbine += workday[:(day_range_cnt - len(conbine)) % 7]
+    return conbine.count('1')
 
 def is_over_lap(yyyymmdd_sten1sten2):
     """2つの期間が重なり合うかどうかを判定する。例えば号機等の重複判定にも使えます\n
@@ -46,7 +51,8 @@ def is_over_lap(yyyymmdd_sten1sten2):
 
 if __name__ == '__main__':
     print(get_now_ym(), get_add_ym(-1), get_add_ym(1))
-    print(convert_to_formal('20190419'), convert_to_ymd(datetime.today()))
+    print(convert_to_formal('20190419'))
+    print(convert_to_ymd(datetime.today()))
     print(count_work_day('20190406', '20190430'))
     print(is_over_lap('20190401-20190430,20190410-20190420'))
     print(is_over_lap('20190401-20190415,20190410-20190430'))
