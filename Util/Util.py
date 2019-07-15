dc18eb5ee1982adc2ee675420e3a39e92f406a85
yoshi <yoshitakaOkada0214@gmail.com>
2019-07-15 20:53:04 +0900

    pylint update

--- Util/Util.py
+++ Util/Util.py
@@ -1,22 +1,31 @@
-import pyodbc
-import pandas as pd
+"""
+    SQLServerに接続します（固定値なので適宜書き換えてください）
+    接続db: (localhostの)SQLExpress
+    接続db: db
+    UID: python
+    PWD: python
+"""
 import datetime
+import pandas as pd
+import pyodbc
 
-# Action Query
-def ExecuteSQLBySQLServer(sql):
-    con = pyodbc.connect(r'DRIVER={SQL Server};SERVER=localhost\SQLExpress;DATABASE=db;UID=python;PWD=python;')
+def execute_sql_by_sqlserver(sql):
+    """Action Query"""
+    con_text = r'DRIVER={SQL Server};SERVER=localhost\SQLExpress;DATABASE=db;UID=python;PWD=python;'
+    con = pyodbc.connect(con_text)
     cur = con.cursor()
     cur.execute(sql)
     con.commit()
     con.close()
 
-# Read Query
-def ReadQueryBySQLServer(sql):
-    con = pyodbc.connect(r'DRIVER={SQL Server};SERVER=localhost\SQLExpress;DATABASE=db;UID=python;PWD=python;')
-    df = pd.io.sql.read_sql(sql,con)
+def read_query_by_sqlserver(sql):
+    """Read Query"""
+    con_text = r'DRIVER={SQL Server};SERVER=localhost\SQLExpress;DATABASE=db;UID=python;PWD=python;'
+    con = pyodbc.connect(con_text)
+    df = pd.io.sql.read_sql(sql, con)
     con.close()
-    return(df)
+    return df
 
-# 2019-02-06T06:00:00+09:00 > 2019/02/06 06:00:00
-def ConvertIso2YMDHMS(v):
-    return(datetime.datetime.fromisoformat(v).strftime("%Y/%m/%d %H:%M:%S"))
+def convert_iso_to_ymdhms(iso_ymdhms):
+    """タイムゾーンつきの文字列のフォーマットを変える 2019-02-06T06:00:00+09:00 -> 2019/02/06 06:00:00"""
+    return datetime.datetime.fromisoformat(iso_ymdhms).strftime("%Y/%m/%d %H:%M:%S")
--- Util/util_ym.py
+++ Util/util_ym.py
@@ -1,34 +1,35 @@
-from datetime import datetime, date, timedelta
+"""年月操作系の関数をまとめました"""
+from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-def GetNowYM():
+def get_now_ym():
     """ systemYMを返します """
-    d = datetime.today()
-    return datetime.strftime(d, '%Y%m')
+    ret = datetime.today()
+    return datetime.strftime(ret, '%Y%m')
 
-def GetAddYM(i):
+def get_add_ym(i):
     """ systemYMに i を加味した値を返します。\n
     例: 2019年04月を現在として -1: 201903, +1: 201905 """
-    d = datetime.today() + relativedelta(months=i)
-    return datetime.strftime(d, '%Y%m')
-    
-def ConvertToFormal(yyyymmdd):
+    ret = datetime.today() + relativedelta(months=i)
+    return datetime.strftime(ret, '%Y%m')
+
+def convert_to_formal(yyyymmdd):
     """ yyyymmddからyyyy/mm/ddへ変換 """
     return datetime.strptime(yyyymmdd, '%Y%m%d').strftime('%Y/%m/%d')
 
-def ConvertToYMD(date_formal):
+def convert_to_ymd(date_formal):
     """ yyyy/mm/ddからyyyymmddへ変換 """
     return datetime.strftime(date_formal, '%Y%m%d')
 
-def CountWorkday(yyyymmdd_st, yyyymmdd_en, workday='1111100'):
+def count_work_day(yyyymmdd_st, yyyymmdd_en, workday='1111100'):
     """ stからenの範囲のうちworkdayが1の場所をカウントして返す。
     例えばworkdayの1111100は土日が休みであることを表す"""
     # ex: between 2019/4/6(sat) and 2019/4/30(tue)
-    d = (datetime.strptime(yyyymmdd_st, '%Y%m%d'), datetime.strptime(yyyymmdd_en, '%Y%m%d'))
+    ret = (datetime.strptime(yyyymmdd_st, '%Y%m%d'), datetime.strptime(yyyymmdd_en, '%Y%m%d'))
     # retValue: 00
-    retValue = workday[d[0].weekday():]
+    retValue = workday[ret[0].weekday():]
     # range are 25 days
-    d_range_cnt = (d[1]-d[0]).days + 1
+    d_range_cnt = (ret[1]-ret[0]).days + 1
     # retValue: 00 + (1111100 * multiple)
     multiple = (d_range_cnt - len(retValue)) // 7
     retValue += workday * multiple
@@ -36,15 +37,19 @@ def CountWorkday(yyyymmdd_st, yyyymmdd_en, workday='1111100'):
     retValue += workday[:(d_range_cnt - len(retValue)) % 7]
     return retValue.count('1')
 
-def IsOverlap(yyyymmdd_sten1sten2):
+def is_over_lap(yyyymmdd_sten1sten2):
     """2つの期間が重なり合うかどうかを判定する。例えば号機等の重複判定にも使えます\n
         例：20190401-20190415,20190410-20190430 のように指定します"""
     ymd1 = yyyymmdd_sten1sten2.split(',')[0].split('-')
     ymd2 = yyyymmdd_sten1sten2.split(',')[1].split('-')
-    return ymd2[0] <= ymd1[1] and ymd1[0] <=ymd2[1]
+    return ymd2[0] <= ymd1[1] and ymd1[0] <= ymd2[1]
 
 if __name__ == '__main__':
-    print(GetNowYM(),GetAddYM(-1),GetAddYM(1))
-    print(ConvertToFormal('20190419'),ConvertToYMD(datetime.today()))
-    print(CountWorkday('20190406','20190430'))
-    print(IsOverlap('20190401-20190430,20190410-20190420'),IsOverlap('20190401-20190415,20190410-20190430'),IsOverlap('20190410-20190430,20190401-20190415'),IsOverlap('20190401-20190409,20190410-20190430'),IsOverlap('20190410-20190430,20190401-20190409'))
+    print(get_now_ym(), get_add_ym(-1), get_add_ym(1))
+    print(convert_to_formal('20190419'), convert_to_ymd(datetime.today()))
+    print(count_work_day('20190406', '20190430'))
+    print(is_over_lap('20190401-20190430,20190410-20190420'))
+    print(is_over_lap('20190401-20190415,20190410-20190430'))
+    print(is_over_lap('20190410-20190430,20190401-20190415'))
+    print(is_over_lap('20190401-20190409,20190410-20190430'))
+    print(is_over_lap('20190410-20190430,20190401-20190409'))
