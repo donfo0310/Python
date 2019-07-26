b416a275eb0b830c87947c69b07cda1ac76a2d23
yoshi <yoshitakaOkada0214@gmail.com>
2019-07-26 20:41:45 +0900

    db reset

--- VNM/step_log.sql
+++ VNM/step_log.sql
@@ -1,3 +1,6 @@
+-- DjangoでDBを消去して再migrationする
+-- https://qiita.com/riz666/items/59352c336398e0321fc2
+
 -- SQLite3 チートシート https://qiita.com/sotetsuk/items/cd2aeae4ba7e72faad47
 -- 設定を表示する
 .show
@@ -7,6 +10,7 @@
 
 -- SQLite3を実行する位置
 VNM> sqlite3 mysite/db.sqlite3
+VNM/mysite> sqlite3 db.sqlite3
 
 -- Djangoでテーブルを作成するとidが勝手につくため、idがないcsvデータではSQLiteでimportできない
 -- そこでいったん「temp_import_industry」という一時テーブルを作り、そこからinsertを発行する
@@ -18,36 +22,13 @@ delete from vietnam_research_industry where strftime('%Y%m', pub_date) = strftim
 .mode csv
 .import ../data/industry.csv temp_import_industry
 
--- tempテーブルからデータを作る（ヘッダーと最終行の空行がちゃんと抜けてたのはテーブル定義のおかげ？）
-insert into 
-    vietnam_research_industry (
-        market_code
-        , symbol
-        , company_name
-        , industry1
-        , industry2
-        , marketcap
-        , marketcap_percentage
-        , pub_date
-    )
-select 
-    Market_code
-    , Symbol
-    , Company_name
-    , industry1
-    , industry2
-    , marketcap
-    , marketcap / (select Sum(marketcap) from temp_import_industry)
-    , strftime('%Y-%m-%d', CURRENT_DATE)
-from temp_import_industry;
-
 -- 一時テーブル削除
 drop table temp_import_industry;
 
 -- 確認
-select * from vietnam_research_industry;
+select * from vietnam_research_industry limit 30;
 
 -- グループ集計
 select COUNT(1) from vietnam_research_industry;
 select industry1, COUNT(1) from vietnam_research_industry group by industry1;
-select industry1, SUM(marketcap), SUM(marketcap_percentage) from vietnam_research_industry group by industry1;
+select industry1, SUM(marketcap), SUM(marketcap_per) from vietnam_research_industry group by industry1;
