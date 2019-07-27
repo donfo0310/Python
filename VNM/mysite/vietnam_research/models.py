f9fd8066e56231d9ff1638c75a97ebd37bc26c4d
yoshi <yoshitakaOkada0214@gmail.com>
2019-07-27 10:36:37 +0900

    産業1-3区分のテーブルを追加

--- VNM/mysite/vietnam_research/models.py
+++ VNM/mysite/vietnam_research/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 class Industry(models.Model):
-    """ここに定義したものがフィールド項目です"""
+    """viet-kabuで取得できる業種つき個社情報"""
     market_code = models.CharField(max_length=4)
     symbol = models.CharField(max_length=10)
     company_name = models.CharField(max_length=50)
@@ -12,3 +12,8 @@ class Industry(models.Model):
     marketcap = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
     marketcap_per = models.DecimalField(max_digits=4, decimal_places=1, default=0.0)
     pub_date = models.DateTimeField('date published')
+
+class IndustryClassification(models.Model):
+    """viet-kabuの産業名を産業区分1-3に"""
+    industry1 = models.CharField(max_length=10)
+    industry_class = models.IntegerField()
--- VNM/step_log.sql
+++ VNM/step_log.sql
@@ -14,21 +14,40 @@ VNM/mysite> sqlite3 db.sqlite3
 
 -- Djangoでテーブルを作成するとidが勝手につくため、idがないcsvデータではSQLiteでimportできない
 -- そこでいったん「temp_import_industry」という一時テーブルを作り、そこからinsertを発行する
-delete from vietnam_research_industry;
+DELETE FROM vietnam_research_industry;
 
-delete from vietnam_research_industry where strftime('%Y%m', pub_date) = strftime('%Y%m', 'now');
+DELETE FROM vietnam_research_industry WHERE strftime('%Y%m', pub_date) = strftime('%Y%m', 'now');
 
 -- SQLite3でのCSVファイルのインポート https://qiita.com/Kunikata/items/61b5ee2c6a715f610493
 .mode csv
 .import ../data/industry.csv temp_import_industry
 
 -- 一時テーブル削除
-drop table temp_import_industry;
+DROP TABLE temp_import_industry;
 
 -- 確認
-select * from vietnam_research_industry limit 30;
+SELECT * FROM vietnam_research_industry LIMIT 30;
 
 -- グループ集計
-select COUNT(1) from vietnam_research_industry;
-select industry1, COUNT(1) from vietnam_research_industry group by industry1;
-select industry1, SUM(marketcap), SUM(marketcap_per) from vietnam_research_industry group by industry1;
+SELECT COUNT(1) FROM vietnam_research_industry;
+SELECT industry1, COUNT(1) FROM vietnam_research_industry GROUP BY industry1;
+SELECT industry1, SUM(marketcap), SUM(marketcap_per) FROM vietnam_research_industry GROUP BY industry1;
+
+-- Industryclassification マスタ
+INSERT INTO vietnam_research_industryclassification VALUES (1, '農林水産業', 1);
+INSERT INTO vietnam_research_industryclassification VALUES (2, '建設業', 2);
+INSERT INTO vietnam_research_industryclassification VALUES (3, '製造業', 2);
+INSERT INTO vietnam_research_industryclassification VALUES (4, '鉱業', 2);
+INSERT INTO vietnam_research_industryclassification VALUES (5, 'サービス業', 3);
+INSERT INTO vietnam_research_industryclassification VALUES (6, '不動産業', 3);
+INSERT INTO vietnam_research_industryclassification VALUES (7, '商業', 3);
+INSERT INTO vietnam_research_industryclassification VALUES (8, '情報通信業', 3);
+INSERT INTO vietnam_research_industryclassification VALUES (9, '運輸・物流業', 3);
+INSERT INTO vietnam_research_industryclassification VALUES (10, '金融業', 3);
+INSERT INTO vietnam_research_industryclassification VALUES (11, '電気・ガス業', 3);
+-- 確認
+SELECT c.industry_class || '|' || i.industry1
+FROM vietnam_research_industry i INNER JOIN vietnam_research_industryclassification c
+ON i.industry1 = c.industry1
+GROUP BY i.industry1, c.industry_class
+ORDER BY c.industry_class, i.industry1;
