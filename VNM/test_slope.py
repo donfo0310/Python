4dbdc4b6e5d98e17c5072f63ec9d469ec446fa34
yoshi <yoshitakaOkada0214@gmail.com>
2019-09-01 23:39:14 +0900

    update

--- VNM/test_slope.py
+++ VNM/test_slope.py
@@ -15,45 +15,51 @@ improvement(top5):
     df.groupby(by=["symbol"]).mean().loc[lambda x: x["per"]> 1]
 """
 
-# import sqlite3
+import sqlite3
 import pandas as pd
 from scipy.stats import linregress
 
 # stackoverflow
 X = pd.DataFrame({
     'entity':['a', 'a', 'b', 'b', 'b'],
-    'year':[19990101, 20040101, 20030101, 20070101, 20140101],
+    'year':["19990101", "20040101", "20030101", "20070101", "20140101"],
     'value':[2, 5, 3, 2, 7]})
+X.year = X.year.astype(int) # 型変換すれば通る
 print(X)
+print(X.dtypes)
 print(X.groupby('entity').apply(lambda v: linregress(v.year, v.value)[0]))
 
 # having value > 2
 print(X.groupby('entity').mean().loc[lambda v: v.value > 2])
 
 # またチャレンジしようね
-# CON = sqlite3.connect('mysite/db.sqlite3')
-# SQL = '''DELETE FROM vietnam_research_dailytop5'''
-# CUR = CON.cursor()
-# CUR.execute(SQL)
-# AGG = pd.read_sql(
-#     '''
-#     SELECT
-#           c.industry_class || '|' || i.industry1 AS ind_name
-#         , i.market_code
-#         , i.symbol
-#         , AVG(trade_price_of_a_day) AS trade_price_of_a_day
-#         , AVG(i.per) AS per
-#     FROM vietnam_research_industry i INNER JOIN vietnam_research_industryclassification c
-#     ON i.industry1 = c.industry1
-#     GROUP BY ind_name, i.market_code, i.symbol
-#     HAVING per >1;
-#     '''
-#     , CON)
-# # criteria
-# CRITERIA = []
-# CRITERIA.append({"by": ['trade_price_of_a_day', 'per'], "order": False})
-# CRITERIA.append({"by": ['ind_name', 'trade_price_of_a_day', 'per'], "order": [True, False, False]})
-# # Sort descending and get top 5
+CON = sqlite3.connect('mysite/db.sqlite3')
+AGG = pd.read_sql(
+    '''
+    SELECT
+          c.industry_class || '|' || i.industry1 AS ind_name
+        , i.market_code
+        , i.symbol
+        , CAST(REPLACE(DATE(i.pub_date), '-', '') AS int) AS pub_date
+        , trade_price_of_a_day
+        , i.per
+    FROM vietnam_research_industry i INNER JOIN vietnam_research_industryclassification c
+    ON i.industry1 = c.industry1;
+    '''
+    , CON)
+print(AGG.dtypes)
+
+# linregress
+AGG = AGG.groupby('symbol').apply(lambda v: linregress(v.pub_date, v.trade_price_of_a_day)[0])
+print(AGG)
+# having value > 2
+# print(AGG.groupby('symbol').mean().loc[lambda v: v.per > 2])
+
+# criteria
+CRITERIA = []
+CRITERIA.append({"by": ['trade_price_of_a_day', 'per'], "order": False})
+CRITERIA.append({"by": ['ind_name', 'trade_price_of_a_day', 'per'], "order": [True, False, False]})
+# Sort descending and get top 5
 # AGG = AGG.sort_values(by=CRITERIA[0]["by"], ascending=CRITERIA[0]["order"])
 # AGG = AGG.groupby('ind_name').head()
 # # Sort descending and insert table
