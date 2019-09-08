2d0fbc45905733d4c6b081d903d923f4da8abaf7
yoshi <yoshitakaOkada0214@gmail.com>
2019-09-08 11:56:29 +0900

    app nameを追加

--- VNM/mysite/vietnam_research/urls.py
+++ VNM/mysite/vietnam_research/urls.py
@@ -1,9 +1,11 @@
+"""docstring"""
 from django.urls import path
 
 # STEP1: 現在のフォルダの「views.py」を import する！さっき編集したやつ！
 from . import views
 
 # STEP2: views.py には「index」という関数を作りましたね！それを呼んでます
+app_name = 'vnm'
 urlpatterns = [
     path('', views.index, name='index'),
 ]
