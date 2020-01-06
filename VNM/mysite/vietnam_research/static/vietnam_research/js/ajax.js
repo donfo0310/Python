2364e78da480bcdc811d1ddb699ff729946b7cd2
yoshi <yoshitakaOkada0214@gmail.com>
2020-01-07 00:09:06 +0900

    update

--- VNM/mysite/vietnam_research/static/vietnam_research/js/ajax.js
+++ VNM/mysite/vietnam_research/static/vietnam_research/js/ajax.js
@@ -4,6 +4,9 @@ function likes(event, user_id, article_id) {
     var pressed = (event.target.getAttribute("aria-pressed") === "true");
     event.target.setAttribute("aria-pressed", !pressed);
 
+    // likes count up/down
+    console.log(event.currentTarget.getElementsByTagName('span')[0].innerHTML.slice(1).split(')')[0]);
+
     // ajax: using pure javascript
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/vietnam_research/likes/' + user_id + '/' + article_id);
