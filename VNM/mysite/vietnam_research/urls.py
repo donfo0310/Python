9e61ed16f0bd0e56624e6f6b27be2fb720e73cd7
yoshi <yoshitakaOkada0214@gmail.com>
2020-01-08 23:03:15 +0900

    update

--- VNM/mysite/vietnam_research/urls.py
+++ VNM/mysite/vietnam_research/urls.py
@@ -1,12 +1,11 @@
 """docstring"""
 from django.urls import path
-
-# STEP1: 現在のフォルダの「views.py」を import する！さっき編集したやつ！
 from . import views
 
-# STEP2: views.py には「index」という関数を作りましたね！それを呼んでます
 app_name = 'vnm'
 urlpatterns = [
+    # index.htmlがリクエストされたときは views.index の処理に
     path('', views.index, name='index'),
+    # いいね！がリクエストされたときは views.likes の処理に 引数[ユーザID, 記事ID] を渡します
     path('likes/<int:user_id>/<int:article_id>', views.likes, name='likes'),
 ]
