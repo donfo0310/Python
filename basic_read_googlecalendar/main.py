d95feb5688dfd67156a21111958f02b1d217fc7c
yoshi <yoshitakaOkada0214@gmail.com>
2020-02-17 20:53:20 +0900

    update

--- basic_read_googlecalendar/main.py
+++ basic_read_googlecalendar/main.py
@@ -1,11 +1,12 @@
+"""docstring"""
 import datetime
 import pickle
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-import lib.Util
 import pandas as pd
+import lib.Util
 
 # If modifying these scopes, delete the file token.pickle.
 # SCOPESを変える必要があるときは 'token.pickle' を消してね（すぐ下で初回認証時に勝手に作られるファイル）
@@ -47,37 +48,36 @@ def main():
     # Google カレンダーでは、夏時間による問題を回避するため、協定世界時（UTC）を採用しています。
     # 作成した予定は UTC に変換されますが、常にそのユーザーの現地時刻で表示されます。
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
+    events_result = service.events().list(calendarId='primary', timeMin=now, \
+                                        maxResults=10, singleEvents=True, \
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
-    df = pd.DataFrame(index=[], columns=['ymdhms', 'summary'])
+    data = pd.DataFrame(index=[], columns=['ymdhms', 'summary'])
     if not events:
         print('No upcoming events found.')
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
-        series = pd.Series([lib.Util.ConvertIso2YMDHMS(start), event['summary']], index=df.columns)
-        df = df.append(series, ignore_index = True)
+        series = pd.Series(
+            [lib.Util.ConvertIso2YMDHMS(start), event['summary']], index=data.columns)
+        data = data.append(series, ignore_index=True)
 
     # データ削除クエリを発行
-    SQL_TEMPLATE = "DELETE FROM [dbo].[Calendar]"   # SQL原本
-    editSql = SQL_TEMPLATE                          # SQL原本に置換をかける
-    lib.Util.ExecuteSQLBySQLServer(editSql)      # DELETE文の発行
+    editsql = "DELETE FROM [dbo].[Calendar]"    # SQL原本に置換をかける
+    lib.Util.ExecuteSQLBySQLServer(editsql)     # DELETE文の発行
 
     # データ追加クエリを発行
-    SQL_TEMPLATE = "INSERT INTO [dbo].[Calendar]([ymdhms],[summary]) VALUES ('{0}','{1}')"
-    for line in df.values:
-        editSql = SQL_TEMPLATE                      # SQL原本
-        for i,col in enumerate(line):               # SQL原本に置換をかける
-            editSql = editSql.replace('{' + str(i) + '}', col)
-        lib.Util.ExecuteSQLBySQLServer(editSql)  # INSERT文の発行
+    for line in data.values:
+        editsql = "INSERT INTO [dbo].[Calendar]([ymdhms],[summary]) VALUES ('{0}','{1}')"   # SQL原本
+        for i, col in enumerate(line):               # SQL原本に置換をかける
+            editsql = editsql.replace('{' + str(i) + '}', col)
+        lib.Util.ExecuteSQLBySQLServer(editsql)  # INSERT文の発行
 
     # 選択クエリを発行
-    SQL_TEMPLATE = "SELECT FORMAT(A.ymdhms,'yyyy-MM-dd HH:mm') AS ymdhms, A.summary FROM [dbo].[Calendar] AS A" # SQL原本
-    editSql = SQL_TEMPLATE                          # SQL原本に置換をかける
-    df = lib.Util.ReadQueryBySQLServer(editSql)  # SELECT文の発行
-    for line in df.values:
+    editsql = \
+        "SELECT FORMAT(A.ymdhms,'yyyy-MM-dd HH:mm') ymdhms, A.summary FROM [dbo].[Calendar] AS A"
+    data = lib.Util.ReadQueryBySQLServer(editsql)  # SELECT文の発行
+    for line in data.values:
         print(','.join(line))                       # SQL結果を調理して提供
 
 if __name__ == '__main__':
